21e4362d02bcc9b91617e6ead31e7754b545b101
Elizaveta Bolshova <bolshovaeliza@gmail.com>
2025-02-02 14:44:29 +0300

    add methods to dialog tree

---
+++ src/model/DialogTree.h
@@ -0,0 +1,27 @@
+#ifndef __SRC_MODEL_DIALOGTREE_H__
+#define __SRC_MODEL_DIALOGTREE_H__
+
+#include "Choice.h"
+#include <iostream>
+
+class DialogTree {
+private:
+  struct Node {
+    Choice value;
+    Node *parent = nullptr;
+    Node *child = nullptr;
+  };
+  Node *root = nullptr;
+  Node *pointer = nullptr;
+
+public:
+  Choice getPointerValue();
+  bool isPointerNull();
+  void pointerToChild();
+  void pointerToParent();
+  void pointerToRoot();
+  void addChild(Choice newEl);
+  void printTree();
+};
+
+#endif // __SRC_MODEL_DIALOGTREE_H__
